9cdc096bca120c7e5fbd98c81f5ce78f92f04b80
ubunts hal <tmp@tmp.com>
2021-06-13 22:41:43 +0900

    typo

--- script/index.js
+++ script/index.js
@@ -10,7 +10,7 @@ app.message = 'I have changed the data!';
 var app2 = new Vue({
     el: '#app-2',
     data: {
-        meddage: "You loaded this page on " + new Date().toLocaleString()
+        message: "You loaded this page on " + new Date().toLocaleString()
     }
 })
 
