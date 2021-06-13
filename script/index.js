b354cd8a56e157a2e26b44f7d39aef0125ae7ed7
ubunts hal <tmp@tmp.com>
2021-06-13 22:33:42 +0900

    dynamically

--- script/index.js
+++ script/index.js
@@ -6,3 +6,10 @@ var app = new Vue({
 });
 
 app.message = 'I have changed the data!';
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        meddage: "You loaded this page on " + new Date().toLocaleString()
+    }
+})
